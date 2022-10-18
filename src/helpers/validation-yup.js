0dcd8c597c3225fabcfc689bad993d86184ce9a1
Stanislav-PSV <stanislav.psv521@gmail.com>
2022-10-18 22:04:33 +0300

    03

--- src/helpers/validation-yup.js
+++ src/helpers/validation-yup.js
@@ -1,6 +1,5 @@
 import * as yup from 'yup';
-const phoneRegExp =
-	/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const phoneRegExp = /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
 
 const nameRegExp = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
 
@@ -8,16 +7,16 @@ export const schema = () =>
 	yup.object().shape({
 		name: yup
 			.string()
+			.required('Required')
 			.matches(
 				nameRegExp,
 				"Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-			)
-			.required(),
+			),
 		number: yup
 			.string()
+			.required('Required')
 			.matches(
 				phoneRegExp,
 				'Phone number must be digits and can contain spaces, dashes, parentheses and can start without +'
 			)
-			.required(),
 	});
