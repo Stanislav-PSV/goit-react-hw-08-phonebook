8be67b8367beeaccd2ccd09b3e7beb0372538851
Stanislav-PSV <92394363+Stanislav-PSV@users.noreply.github.com>
2022-10-18 22:40:53 +0300

    06

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -16,9 +16,9 @@ export const Contact = styled.p`
 `;
 
 export const ButtonClose = styled.button`
-	padding: 0 5px 5px 5px;
-	font-size: 20px;
-	width: 20px;
+	padding: 5px 5px 5px 5px;
+	font-size: 10px;
+	width: 30px;
 	background-color: inherit;
 	border: 1px solid var(--color-button);
 	border-radius: 5px;
